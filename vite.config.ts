45c19c6a2cd9d1a45ad60618e0880e0d356b6684
joostmclaassen <129854163+joostmclaassen@users.noreply.github.com>
2024-06-06 12:33:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TODSS_SA438/',
+  base: '/RAL-Contrast-checker/',
   plugins: [react()],
 })
